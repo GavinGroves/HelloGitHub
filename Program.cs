48a439c787af23e745967566a459cae5ac3ff233
GavinGroves <649941598@qq.com>
2023-02-09 21:55:05 +0800

    第一次github

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_My_Second_C_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Log();
+        }
+    }
